f08a626f5a3a103b22e79d1c1e687b6dc57a77f8
Luizgms <luiz.mendes@sga.pucminas.br>
2024-03-06 15:25:14 -0300

    .class do cliente

--- Codigo/Cliente/src/Servidor.java
+++ Codigo/Cliente/src/Servidor.java
@@ -2,17 +2,17 @@ import java.io.IOException;
 import java.rmi.Naming;
 
 public class Servidor {
-	public Servidor() {
-		try {
-			Dicionario c = new DicionarioImp();
-			Naming.rebind("rmi://localhost/DicionarioService", c);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
+    public Servidor() {
+        try {
+            Dicionario c = new DicionarioImp();
+            Naming.rebind("rmi://localhost/DicionarioService", c);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
-	public static void main(String args[]) throws IOException {
-		new Servidor();
-		System.out.println("Servidor Calculadora em execução.");
-	}
+    public static void main(String args[]) throws IOException {
+        new Servidor();
+        System.out.println("Servidor Dicionario em execução.");
+    }
 }
